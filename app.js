f4be6ac16490c5cd2269950fe845abc9dda11bac
AM0726Github <Margaryanalik7@gmail.com>
2022-01-09 19:26:10 -0800

    using inqurier methods for prompting questions

--- app.js
+++ app.js
@@ -1,17 +1,89 @@
-// Using node.js module file system by calling from app.js 
-const fs = require('fs');
-
-// Implementing generatepage method from source files
-const generatePage = require('./src/page-template.js');
-
+// Implementing inquirer
 const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+            // // Using node.js module file system by calling from app.js 
+            // const fs = require('fs');
 
-const [Name, github] = profileDataArgs;
+            // // Implementing generatepage method from source files
+            // const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(Name,github), err => {
-    if(err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+            // const profileDataArgs = process.argv.slice(2);
 
+            // const [Name, github] = profileDataArgs;
+
+            // const pageHTML = generatePage(Name,github);
+
+            // fs.writeFile('index.html', generatePage(Name,github), err => {
+            //     if(err) throw err;
+            //     console.log('Portfolio complete! Check out index.html to see the output!');
+            // });
+
+// console.log(inquirer);
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
+      }
+    ]);
+  };
+
+const promptProject = () => {
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ]);
+  };
+
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
